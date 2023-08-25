424d8e85f9604c13912604b33a33feedc0885b37
Artemon-x58 <artem1145819@gmail.com>
2023-08-25 20:02:21 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { BtnLoadMore } from './Button/Button';
 
 import { funcRequest } from './js/api';
 import { Loader } from './Loader/Loader';
-export class App extends React.Component {
-  state = {
-    tag: '',
-    images: [],
-    page: 1,
-    loadMore: false,
-    loader: false,
-  };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.tag !== prevState.tag ||
-      this.state.page !== prevState.page
-    ) {
-      this.toggleLoader();
-      funcRequest(this.state.tag, this.state.page)
-        .then(data => {
-          if (data.totalHits === 0) {
-            alert('Oops! No images found. Please enter another word');
-            return;
-          }
-          this.setState(prev => ({
-            images: [...prev.images, ...data.hits],
-            loadMore: this.state.page < Math.ceil(data.totalHits / 12),
-          }));
-        })
-        .catch(err => console.log(err))
-        .finally(() => {
-          this.toggleLoader();
-        });
+export const App = () => {
+  const [tag, setTag] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loadMore, setLoadMore] = useState(false);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    if (tag !== '' || page !== 1) {
+      toggleLoader();
+
+      function getPictures() {
+        funcRequest(tag, page)
+          .then(data => {
+            if (data.totalHits === 0) {
+              alert('Oops! No images found. Please enter another word');
+              return;
+            }
+            setImages(prevImages => [...prevImages, ...data.hits]);
+            setLoadMore(page < Math.ceil(data.totalHits / 12));
+          })
+          .catch(err => {
+            console.log(err);
+          })
+          .finally(() => {
+            toggleLoader();
+          });
+      }
+      getPictures();
     }
-  }
-
-  toggleLoader = () => {
-    this.setState(prev => ({
-      loader: !prev.loader,
-    }));
+  }, [tag, page]);
+  const toggleLoader = () => {
+    setLoader(prevLoader => !prevLoader);
   };
-  onChangeTag = newTag => {
-    this.setState({ tag: newTag, page: 1, images: [], loadMore: false });
-  };
-
-  onChangePage = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const onChangeTag = newTag => {
+    setTag(newTag);
+    setPage(1);
+    setImages([]);
+    setLoadMore(false);
   };
 
-  render() {
-    return (
+  const onChangePage = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <>
+      <Searchbar onSubmit={onChangeTag} />;{loader && <Loader />}
       <>
-        <Searchbar onSubmit={this.onChangeTag} />;
-        {this.state.loader && <Loader />}
-        <>
-          {this.state.images.length > 0 && (
-            <ImageGallery
-              onRequest={this.state.tag}
-              onCreateImg={this.state.images}
-            />
-          )}
+        {images.length > 0 && (
+          <ImageGallery onRequest={tag} onCreateImg={images} />
+        )}
 
-          {this.state.loadMore && <BtnLoadMore onClick={this.onChangePage} />}
-        </>
+        {loadMore && <BtnLoadMore onClick={onChangePage} />}
       </>
-    );
-  }
-}
+    </>
+  );
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../styles.css';
 import { CustomModal } from 'components/Modal/Modal';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ src, alt, srcModal }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
-  toggleModal = () => {
-    this.setState(prev => ({
-      isModalOpen: !prev.isModalOpen,
-    }));
-  };
-
-  render() {
-    return (
-      <li className="ImageGalleryItem-image">
-        <img
-          onClick={this.toggleModal}
-          className="ImageGalleryItem-image"
-          src={this.props.src}
-          alt={this.props.alt}
-        />
-        <CustomModal
-          isOpen={this.state.isModalOpen}
-          isClose={this.toggleModal}
-          srcModal={this.props.srcModal}
-        />
-      </li>
-    );
-  }
-}
+  return (
+    <li className="ImageGalleryItem-image">
+      <img
+        onClick={toggleModal}
+        className="ImageGalleryItem-image"
+        src={src}
+        alt={alt}
+      />
+      <CustomModal
+        isOpen={isModalOpen}
+        isClose={toggleModal}
+        srcModal={srcModal}
+      />
+    </li>
+  );
+};
